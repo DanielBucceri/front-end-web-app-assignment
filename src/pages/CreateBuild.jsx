356c548f344ec4ae836ad08135070f36ea81f6dd
Daniel <gcas012125@coderacademy.edu.au>
2025-06-17 00:48:49 +1000

    style: improve Pokemon image display and update create build page title

--- src/pages/CreateBuild.jsx
+++ src/pages/CreateBuild.jsx
@@ -131,23 +131,17 @@ const CreateBuild = () => {
 return (
     <div className="auth-container">
   <div className="auth-card">
-    <h2>Create a New Build</h2>
+    <h2>Create Pokémon Build</h2>
     {showImage && (
-      <img
-        className="pokemon-image"
-        src={spriteUrl}
-        alt={'image of ' + species}
-      />
+      <div className="pokedex-image-container">
+        <img
+          className="pokemon-image"
+          src={spriteUrl}
+          alt={'image of ' + species}
+        />
+      </div>
     )}
     <form className="auth-form" onSubmit={handleSubmit}>
-      {showImage && (
-    <div className="pokedex-image-container">
-                <img
-                  className="pokemon-image"
-                  src={spriteUrl}
-                  alt={'image of ' + species}/>
-              </div>
-      )}
       <div className="error-message">{error}</div>
 
       <div className="form-group">
